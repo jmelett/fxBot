c30e234b9c709515aa3fce10ce56843165cccc05
deso <deso@devnull.com>
2014-07-05 23:09:55 +0100

    introduce new class Program
    Since the beginning a lot of functionality got added to fxBot.py and by
    now it is hard to get a quick overview. This change refactors the code
    by introducing a new class Program which provides methods corresponding
    to actions as specifiable through the different command line arguments.

--- src/fxBot/fxBot.py
+++ src/fxBot/fxBot.py
@@ -19,10 +19,8 @@
 
 """A trading bot for the Forex market using OANDA's REST API."""
 
-from decimal    import Decimal
-from oandapy    import API
-from optparse   import OptionParser
-from statistics import calculateAvg, calculateEMA
+from optparse import OptionParser
+from program  import Program
 
 
 def main():
@@ -43,87 +41,20 @@ def main():
     parser.error("invalid number of arguments")
 
   token = arguments[0]
-  oanda = API(environment="practice", access_token=token)
+  program = Program(token)
 
   if options.list_accounts:
-    idString = "id"
-    nameString = "name"
-    currencyString = "currency"
-    idWidth = len(idString)
-    nameWidth = len(nameString)
-
-    accounts = oanda.get_accounts().get("accounts")
-
-    for account in accounts:
-      idWidth   = max(idWidth,   len(str(account['accountId'])))
-      nameWidth = max(nameWidth, len(str(account['accountName'])))
-
-    print "%s %s %s" % (idString.ljust(idWidth),
-                        nameString.ljust(nameWidth),
-                        currencyString)
-
-    for account in accounts:
-      print "%s %s %s" % (str(account['accountId']).ljust(idWidth),
-                          str(account['accountName']).ljust(nameWidth),
-                          str(account['accountCurrency']))
+    program.listAccounts();
     exit(0)
 
   if options.list_currencies:
     if not options.account_id:
       parser.error("no account ID specified, use --account-id=<account ID>")
 
-    nameString = "name"
-    pipString = "pip"
-    maxUnitsString = "maximum trade units"
-    nameWidth = len(nameString)
-    pipWidth = len(pipString)
-
-    currencies = oanda.get_instruments(options.account_id).get('instruments')
-
-    for currency in currencies:
-      nameWidth = max(nameWidth, len(str(currency['displayName'])))
-      pipWidth  = max(pipWidth,  len(str(currency['pip'])))
-
-    print "%s %s %s" % (nameString.ljust(nameWidth),
-                        pipString.ljust(pipWidth),
-                        maxUnitsString)
-
-    for currency in currencies:
-      print "%s %s %s" % (str(currency['displayName']).ljust(nameWidth),
-                          str(currency['pip']).ljust(pipWidth),
-                          str(currency['maxTradeUnits']))
+    program.listCurrencies(options.account_id);
     exit(0)
 
-    prices = oanda.get_prices(instruments="EUR_USD").get("prices")
-    history = oanda.get_history(instrument="EUR_USD",
-                                granularity="H4",
-                                #start=
-                                #end=datetime.now().isoformat("T"),
-                                #includeFirst=True,
-                                candleFormat="midpoint",
-                                count=20).get("candles")
-
-    # create our own list of dicts with our own set of indices and a reverse ordering where the newest
-    # entries are at the lower indices
-    values = []
-    for value in history:
-      values = [{'time': value['time'],
-                 'open': Decimal(value['openMid']),
-                 'close': Decimal(value['closeMid'])}] + values
-
-    calculateAvg(values, 'open', 'close', 'avg')
-    calculateEMA(values, 10, 'avg', 'ema10')
-    calculateEMA(values, 20, 'avg', 'ema20')
-
-    print "EUR/USD:"
-    print "current prices: ask=%s, bid=%s" % (prices[0]['ask'], prices[0]['bid'])
-    print "historic data:"
-    print "<time>                       <EMA 20>                          <EMA 10>"
-
-    for value in values:
-      print "%s: %s vs. %s" % (value['time'],
-                               value['ema20'] if 'ema20' in value else '<nil>',
-                               value['ema10'] if 'ema10' in value else '<nil>')
+  program.run()
 
 
 if __name__ == '__main__':
---
+++ src/fxBot/program.py
@@ -0,0 +1,117 @@
+# program.py
+
+#/***************************************************************************
+# *   Copyright (C) 2014 Daniel Mueller                                     *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+from decimal    import Decimal
+from oandapy    import API
+from statistics import calculateAvg, calculateEMA
+
+
+class Program:
+  def __init__(self, token):
+    """Create new Program object using the given access token.
+
+      Parameters:
+        token  An access token to use for interacting with OANDA's REST API.
+    """
+    self.__api = API(environment="practice", access_token=token)
+
+
+  def listAccounts(self):
+    """List all available accounts."""
+    idString = "id"
+    nameString = "name"
+    currencyString = "currency"
+    idWidth = len(idString)
+    nameWidth = len(nameString)
+
+    accounts = self.__api.get_accounts().get("accounts")
+
+    for account in accounts:
+      idWidth   = max(idWidth,   len(str(account['accountId'])))
+      nameWidth = max(nameWidth, len(str(account['accountName'])))
+
+    print "%s %s %s" % (idString.ljust(idWidth),
+                        nameString.ljust(nameWidth),
+                        currencyString)
+
+    for account in accounts:
+      print "%s %s %s" % (str(account['accountId']).ljust(idWidth),
+                          str(account['accountName']).ljust(nameWidth),
+                          str(account['accountCurrency']))
+
+
+  def listCurrencies(self, account_id):
+    """Print a list of all available currencies for the account with the given ID.
+
+      Parameters:
+        account_id  The ID of the account for which to list the available currencies.
+    """
+    nameString = "name"
+    pipString = "pip"
+    maxUnitsString = "maximum trade units"
+    nameWidth = len(nameString)
+    pipWidth = len(pipString)
+
+    currencies = self.__api.get_instruments(account_id).get('instruments')
+
+    for currency in currencies:
+      nameWidth = max(nameWidth, len(str(currency['displayName'])))
+      pipWidth  = max(pipWidth,  len(str(currency['pip'])))
+
+    print "%s %s %s" % (nameString.ljust(nameWidth),
+                        pipString.ljust(pipWidth),
+                        maxUnitsString)
+
+    for currency in currencies:
+      print "%s %s %s" % (str(currency['displayName']).ljust(nameWidth),
+                          str(currency['pip']).ljust(pipWidth),
+                          str(currency['maxTradeUnits']))
+
+
+  def run(self):
+    prices = self.__api.get_prices(instruments="EUR_USD").get("prices")
+    history = self.__api.get_history(instrument="EUR_USD",
+                                     granularity="H4",
+                                     #start=
+                                     #end=datetime.now().isoformat("T"),
+                                     #includeFirst=True,
+                                     candleFormat="midpoint",
+                                     count=20).get("candles")
+
+    # create our own list of dicts with our own set of indices and a reverse ordering where the newest
+    # entries are at the lower indices
+    values = []
+    for value in history:
+      values = [{'time': value['time'],
+                 'open': Decimal(value['openMid']),
+                 'close': Decimal(value['closeMid'])}] + values
+
+    calculateAvg(values, 'open', 'close', 'avg')
+    calculateEMA(values, 10, 'avg', 'ema10')
+    calculateEMA(values, 20, 'avg', 'ema20')
+
+    print "EUR/USD:"
+    print "current prices: ask=%s, bid=%s" % (prices[0]['ask'], prices[0]['bid'])
+    print "historic data:"
+    print "<time>                       <EMA 20>                          <EMA 10>"
+
+    for value in values:
+      print "%s: %s vs. %s" % (value['time'],
+                               value['ema20'] if 'ema20' in value else '<nil>',
+                               value['ema10'] if 'ema10' in value else '<nil>')

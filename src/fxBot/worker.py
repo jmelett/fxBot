873f6cfb166b7553849364606eee7a401a889958
deso <deso@devnull.com>
2014-07-06 23:51:42 +0100

    push all actual work functionality into the worker
    Now that the worker is in place, we can also push more of the event
    processing into this object away from the streaming threads. This way,
    the worker is the central point of processing and forms the starting
    point for all event based logic.

--- src/fxBot/worker.py
+++ src/fxBot/worker.py
@@ -18,6 +18,7 @@
 # ***************************************************************************/
 
 from threading import Thread, Event
+from logging   import info, warn
 from Queue     import Queue
 
 
@@ -33,6 +34,79 @@ class Worker(Thread):
     self.__queue = Queue()
     self.__destroy = Event()
 
+  def handleTick(self, tick):
+    '''Handle a tick received from the Oanda server.
+
+      Parameters:
+        tick  Dictionary containing timestamp, bid and ask prices, and more.
+    '''
+
+    # tick:
+    # instrument Name of the instrument.
+    # time       Time in a valid datetime format.
+    # bid        Bid price
+    # ask        Ask price
+    info("%s: %s ask=%s, bid=%s" % (tick['time'], tick['instrument'], tick['ask'], tick['bid']))
+    # process the data item!
+
+
+  def handleEvent(self, event):
+    '''Handle an event received from the Oanda server.
+
+      Parameters:
+        event  Dictionary defining an event. The value of the 'type' should be investigated to get
+        more details on the type of event.
+    '''
+
+    transaction = data['transaction']
+
+    # transaction:
+    # id                       Transaction ID
+    # accountId                Account ID
+    # time                     Time in a valid datetime format.
+    # type                     Transaction type. Possible values: ORDER_FILLED, STOP_LOSS_FILLED,
+    #                          TAKE_PROFIT_FILLED, TRAILING_STOP_FILLED, MARGIN_CLOSEOUT,
+    #                          ORDER_CANCEL, MARGIN_CALL_ENTER, MARGIN_CALL_EXIT.
+    # instrument               The name of the instrument.
+    # side                     The direction of the action performed on the account, possible
+    #                          values are: buy, sell.
+    # units                    The amount of units involved.
+    # price                    The execution or requested price.
+    # lowerBound               The minimum execution price.
+    # upperBound               The maximum execution price.
+    # takeProfitPrice          The price of the take profit.
+    # stopLossPrice            The price of the stop loss.
+    # trailingStopLossDistance The distance of the trailing stop in pips, up to one decimal place.
+    # pl                       The profit and loss value.
+    # interest                 The interest accrued.
+    # accountBalance           The balance on the account after the event.
+    # tradeId                  ID of a trade that has been closed or opened
+    # orderId                  ID of a filled order.
+    # tradeOpened              This object is appended to the json response if a new trade has
+    #                          been created. Trade related fields are: id, units.
+    # tradeReduced             This object is appended to the json response if a trade has been
+    #                          closed or reduced. Trade related fields are: id, units, pl,
+    #                          interest.
+    if transaction['type'] == "ORDER_FILLED":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "STOP_LOSS_FILLED":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "TAKE_PROFIT_FILLED":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "TRAILING_STOP_FILLED":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "MARGIN_CLOSEOUT":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "ORDER_CANCEL":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "MARGIN_CALL_ENTER":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    elif transaction['type'] == "MARGIN_CALL_EXIT":
+      warn("%s: received event: %s" % (transaction['time'], transaction['type']))
+    else:
+      warn("%s: Unknown transaction event received: %s" % (transaction['time'],
+                                                           transaction['type']))
+
 
   def run(self):
     '''Perform the actual work of processing newly incoming events.'''
@@ -43,7 +117,16 @@ class Worker(Thread):
       if self.__destroy.is_set():
         break
 
-      # process the data item!
+      if 'transaction' in data:
+        self.handleEvent(data['transaction'])
+      # not sure if the API allows for heartbeat and transaction events at the same time (I think so),
+      # but we don't care: if there is a transaction event we know something happened and we just
+      # ignore the heartbeat (that's why we use elif)
+      elif 'heartbeat' in data:
+        heartbeat = data['heartbeat']
+        info("%s: heartbeat" % heartbeat['time'])
+      else:
+        self.handleTick(data)
 
 
   def destroy(self):

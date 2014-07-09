f8e932f50f1a585cb4173ca1b708a5df136baefa
deso <deso@devnull.com>
2014-07-09 23:13:40 +0100

    add module for handling times as per RFC3339
    With this change a new module is introduced: datetimeRfc3339 can be used
    to parse dates encoded as specified in RFC3339. Note that currently only
    the relevant subset for our purposes is supported: only times encoded
    for UTC timezone (ending with a 'Z').
    This change also introduces a unit test to verify the parsing is correct
    based on some strings actually received and some made up ones.

---
+++ src/test/testDatetimeRfc3339.py
@@ -0,0 +1,45 @@
+# testDatetimeRfc3339.py
+
+#/***************************************************************************
+# *   Copyright (C) 2014 Daniel Mueller                                     *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+from datetime        import datetime
+from datetimeRfc3339 import parseDate, formatDate
+from unittest        import TestCase, main
+
+
+class TestDatetimeRfc3339(TestCase):
+  def testParse(self):
+    """Test parsing of a couple of dates."""
+    self.assertEqual(parseDate("2014-07-09T00:00:00.000000Z"), datetime(2014, 7, 9, 0, 0, 0, 0))
+    self.assertEqual(parseDate("2014-07-09T01:00:00.000000Z"), datetime(2014, 7, 9, 1, 0, 0, 0))
+    self.assertEqual(parseDate("2014-07-09T12:00:00.000000Z"), datetime(2014, 7, 9, 12, 0, 0, 0))
+    self.assertEqual(parseDate("2014-07-09T12:00:00.000001Z"), datetime(2014, 7, 9, 12, 0, 0, 1))
+    self.assertEqual(parseDate("2014-07-09T19:02:50.000082Z"), datetime(2014, 7, 9, 19, 2, 50, 82))
+
+
+  def testFormat(self):
+    """Test formating a string out of a couple of dates."""
+    self.assertEqual("2014-07-09T00:00:00.000000Z", formatDate(datetime(2014, 7, 9, 0, 0, 0, 0)))
+    self.assertEqual("2014-07-09T01:00:00.000000Z", formatDate(datetime(2014, 7, 9, 1, 0, 0, 0)))
+    self.assertEqual("2014-07-09T12:00:00.000000Z", formatDate(datetime(2014, 7, 9, 12, 0, 0, 0)))
+    self.assertEqual("2014-07-09T12:00:00.000001Z", formatDate(datetime(2014, 7, 9, 12, 0, 0, 1)))
+    self.assertEqual("2014-07-09T19:02:50.000082Z", formatDate(datetime(2014, 7, 9, 19, 2, 50, 82)))
+
+
+if __name__ == '__main__':
+  main()

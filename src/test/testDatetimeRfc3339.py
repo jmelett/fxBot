33008d0423428bc36c6fd7938dd02588a1343f71
deso <deso@devnull.com>
2014-07-12 22:57:54 +0100

    MERGE W/ 121595f2d5aee211f91fb32451df91c43296409d

--- src/test/testDatetimeRfc3339.py
+++ src/test/testDatetimeRfc3339.py
@@ -22,7 +22,7 @@ from datetimeRfc3339 import parseDate, formatDate
 from unittest        import TestCase, main
 
 
-class TestFxBot(TestCase):
+class TestDatetimeRfc3339(TestCase):
   def testParse(self):
     """Test parsing of a couple of dates."""
     self.assertEqual(parseDate("2014-07-09T00:00:00.000000Z"), datetime(2014, 7, 9, 0, 0, 0, 0))
@@ -33,11 +33,13 @@ class TestFxBot(TestCase):
 
 
   def testFormat(self):
+    """Test formating a string out of a couple of dates."""
     self.assertEqual("2014-07-09T00:00:00.000000Z", formatDate(datetime(2014, 7, 9, 0, 0, 0, 0)))
     self.assertEqual("2014-07-09T01:00:00.000000Z", formatDate(datetime(2014, 7, 9, 1, 0, 0, 0)))
     self.assertEqual("2014-07-09T12:00:00.000000Z", formatDate(datetime(2014, 7, 9, 12, 0, 0, 0)))
     self.assertEqual("2014-07-09T12:00:00.000001Z", formatDate(datetime(2014, 7, 9, 12, 0, 0, 1)))
     self.assertEqual("2014-07-09T19:02:50.000082Z", formatDate(datetime(2014, 7, 9, 19, 2, 50, 82)))
+
 
 if __name__ == '__main__':
   main()

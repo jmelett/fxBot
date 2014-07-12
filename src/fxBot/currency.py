d912921c946d023de9a2a21499e9ec7aade412f0
deso <deso@devnull.com>
2014-07-12 20:15:57 +0100

    employ a caching scheme for a currency's historic data
    With this change the history() method of Currency objects does not
    necessarily query the OANDA server on every invocation. Instead, we
    store the last data we retrieved and decide whether it is still current
    based on the granularity of the request, the current time, as well as
    the last time data was actually retrieved from the server.
    The overall goal towards which this change is a first step is to reduce
    the overall amount of connections to the OANDA servers as much as
    possible since there exists a limit of 4 (peak 5) connections per
    second. If this limit is exceeded clients might be subject to throttling
    or other means degrading the quality of service.

--- src/fxBot/currency.py
+++ src/fxBot/currency.py
@@ -18,36 +18,42 @@
 # ***************************************************************************/
 
 from decimal         import Decimal
+from logging         import debug
+from datetime        import datetime, timedelta
 from datetimeRfc3339 import parseDate
 
 
 """Dictionary for mapping granularities from user-friendly ones to OANDA specific ones."""
 _granularities = {
-  '5s':  'S5',  # 5 seconds
-  '10s': 'S10', # 10 seconds
-  '15s': 'S15', # 15 seconds
-  '30s': 'S30', # 30 seconds
-  '1m':  'M1',  # 1 minute
-  # Top of the hour alignment
-  '2m':  'M2',  # 2 minutes
-  '3m':  'M3',  # 3 minutes
-  '5m':  'M5',  # 5 minutes
-  '10m': 'M10', # 10 minutes
-  '15m': 'M15', # 15 minutes
-  '30m': 'M30', # 30 minutes
-  '1h':  'H1',  # 1 hour
-  # Start of day alignment (17:00, Timezone/New York)
-  '2h':  'H2',  # 2 hours
-  '3h':  'H3',  # 3 hours
-  '4h':  'H4',  # 4 hours
-  '6h':  'H6',  # 6 hours
-  '8h':  'H8',  # 8 hours
-  '12h': 'H12', # 12 hours
-  '1d':  'D',   # 1 Day
-  # Start of week alignment (Saturday)
-  '1w':  'W',   # 1 Week
-  # Start of month alignment (First day of the month)
-  '1M':  'M',   # 1 Month
+  # Top of the minute alignment.
+  '5s':  {'string': 'S5',  'time': timedelta(seconds=5)},
+  '10s': {'string': 'S10', 'time': timedelta(seconds=10)},
+  '15s': {'string': 'S15', 'time': timedelta(seconds=15)},
+  '30s': {'string': 'S30', 'time': timedelta(seconds=30)},
+  '1m':  {'string': 'M1',  'time': timedelta(minutes=1)},
+  # Top of the hour alignment.
+  '2m':  {'string': 'M2',  'time': timedelta(minutes=2)},
+  '3m':  {'string': 'M3',  'time': timedelta(minutes=3)},
+  '5m':  {'string': 'M5',  'time': timedelta(minutes=5)},
+  '10m': {'string': 'M10', 'time': timedelta(minutes=10)},
+  '15m': {'string': 'M15', 'time': timedelta(minutes=15)},
+  '30m': {'string': 'M30', 'time': timedelta(minutes=30)},
+  '1h':  {'string': 'H1',  'time': timedelta(hours=1)},
+  # Start of day alignment (17:00, Timezone/New York).
+  '2h':  {'string': 'H2',  'time': timedelta(hours=2)},
+  '3h':  {'string': 'H3',  'time': timedelta(hours=3)},
+  '4h':  {'string': 'H4',  'time': timedelta(hours=4)},
+  '6h':  {'string': 'H6',  'time': timedelta(hours=6)},
+  '8h':  {'string': 'H8',  'time': timedelta(hours=8)},
+  '12h': {'string': 'H12', 'time': timedelta(hours=12)},
+  '1d':  {'string': 'D',   'time': timedelta(hours=24)},
+  # Start of week alignment (Saturday).
+  # TODO: check if one week really is exactly one week in their sense and that we do not have to
+  #       have additional logic for realignment etc.
+  '1w':  {'string': 'W',   'time': timedelta(weeks=1)},
+  # Start of month alignment (First day of the month).
+  # TODO: check if four weeks really are a month in their sense
+  '1M':  {'string': 'M',   'time': timedelta(weeks=4)},
 }
 
 
@@ -82,6 +88,15 @@ class Currency:
     """
     self.__api = api
     self.__currency = currency
+    # dict indexed by the granularity we are looking for
+    # {
+    #   granularity:
+    #   {
+    #     'lastUpdate': datetime,
+    #     'data': [{'time': datetime, 'open': Decimal, 'close': Decimal }]
+    #   }
+    # }
+    self.__historyData = {}
 
 
   def name(self):
@@ -107,6 +122,10 @@ class Currency:
   def history(self, granularity, count):
     """Retrieve past values of a currency.
 
+       Retrieve a certain number of data points from the currency's past. This method employs
+       a caching approach of data previously received from the server to avoid issuing too many
+       requests to the server.
+
       Parameters:
         granularity  The granularity of the historic data to query.
         count        Amount of historic datapoints to retrieve.
@@ -117,13 +136,40 @@ class Currency:
 
       Notes:
         All valid granularities can be found as the keys of the '_granularities' dict object.
+
+      TODO: The caching approach still suffers from one problem: there is a potentially large gap
+            between our system's current time and the server time. The data we receive is aligned
+            based on the server's time. Now because of the time difference it is possible that we
+            return data from our cache although there is already more recent data available on the
+            server.
+            To solve this issue we need to relate the server's time to our's. But there we have the
+            problem that we do not reliably know the server's time (since that data is already
+            aligned and we do not necessarily receive data for the "current" non-aligned time). One
+            possible solution is to inspect all data from the server and update our view of the
+            server's current time from that timestamp. Especially the eventStreamer's heartbeat
+            might provide a reliable source even over the weekend or when markets are closed.
     """
     if not granularity in _granularities:
       raise KeyError("Invalid granularity specified")
 
-    granularity = _granularities[granularity]
+    granularityString = _granularities[granularity]['string']
+    granularityDelta  = _granularities[granularity]['time']
+
+    # check if there is any data for the given granularity
+    if granularityString in self.__historyData:
+      data = self.__historyData[granularityString]
+
+      # check if we got data from a last query that should still be current
+      if datetime.now() - data['lastUpdate'] < granularityDelta:
+        # check if that data contains enough timestamps
+        if len(data['data']) >= count:
+          debug("currency: cache-hit (currency=%s, granularity=%s)"
+                  % (self.name(), granularity))
+          return data['data'][0:count]
+
+    # there is no or to few history data in our cache, get the history from the server
     history = self.__api.get_history(instrument=self.__currency,
-                                     granularity=granularity,
+                                     granularity=granularityString,
                                      #start=...,
                                      #end=datetime.now().isoformat("T") + "Z",
                                      #includeFirst="false",
@@ -138,4 +184,10 @@ class Currency:
                  'open': Decimal(value['openMid']),
                  'close': Decimal(value['closeMid'])}] + values
 
+    # note that we could do much more effort to build the history incrementally by comparing
+    # timestamps etc., but in the end this simple case just replacing all the stale data with all
+    # the new data probably covers all relevant cases we are interested in
+    self.__historyData[granularityString] = {'lastUpdate': datetime.now(), 'data': values}
+    debug("currency: cache-miss (currency=%s, granularity=%s, count=%s)"
+            % (self.name(), granularity, count))
     return values

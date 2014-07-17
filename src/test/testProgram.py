484622be947e9c2aea82db5c640d5ff8f3185fae
deso <deso@devnull.com>
2014-07-17 19:14:39 +0100

    add complete infrastructure test
    This change adds a unit test that verifies the basic infrastructure for
    pushing events from the various sources (rate and event streamer
    threads, and watchdog) works as intended.
    Focus is on execising all main paths so as to catch potential type
    errors and the like. Correct evaluation of the events has to be verified
    by a unit test for each component.

--- src/test/testProgram.py
+++ src/test/testProgram.py
@@ -17,12 +17,33 @@
 # *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 # ***************************************************************************/
 
-from program  import Program
-from unittest import TestCase, main
+from price     import Price
+from program   import Program
+from decimal   import Decimal
+from time      import sleep
+from threading import Thread, Event
+from unittest  import TestCase, main
+from mock      import patch
 
 
 class MockServer:
+  def token(self):
+    """Dummy implementation for the token() method of a real server."""
+    return 'XXXXXXXXXX'
+
+
+  def currentPrices(self, currency):
+    """Dummy implementation for the currentPrices() method of a real server."""
+    return {
+        'instrument': 'XAU_USD',
+        'time': '2014-07-11T20:59:58.718193Z',
+        'ask': 1339.211,
+        'bid': 1336.661,
+      }
+
+
   def accounts(self):
+    """Dummy implementation for the accounts() method of a real server."""
     return [{
         'accountId': 1815754,
         'accountName': 'Primary',
@@ -32,6 +53,7 @@ class MockServer:
 
 
   def instruments(self, account_id, currencies):
+    """Dummy implementation for the instruments() method of a real server."""
     return [{
         'instrument': 'XAU_USD',
         'displayName': 'Gold',
@@ -41,6 +63,7 @@ class MockServer:
 
 
   def trades(self, account_id):
+    """Dummy implementation for the trades() method of a real server."""
     return [{
         'id': 612021234,
         'time': '2014-07-14T17:21:26.000000Z',
@@ -53,6 +76,62 @@ class MockServer:
         'trailingStop': 0,
         'trailingAmount': 0,
       }]
+
+
+class MockStreamer(Thread):
+  def __init__(self, token, queue):
+    Thread.__init__(self)
+    self._destroy = Event()
+    self._queue = queue
+
+
+  def start(self, *args, **kwargs):
+    Thread.start(self)
+
+  def disconnect(self):
+    self._destroy.set()
+
+
+class MockRateStreamer(MockStreamer):
+  def run(self):
+    i = 0
+    while not self._destroy.is_set():
+      if i % 2 == 0:
+        self._queue.put({
+          'instrument': 'XAU_USD',
+          'time': '2014-07-09T00:00:00.000000Z',
+          'ask':  '0.1111111',
+          'bid':  '0.1111110',
+        })
+      else:
+        self._queue.put({
+          'instrument': 'XAU_USD',
+          'time': datetime(2014, 7, 9, 12, 0, 0, 0),
+          'ask':  Price(Decimal('0.1111111'), Decimal('0.001')),
+          'bid':  Price(Decimal('0.1111110'), Decimal('0.001')),
+          'parsed': True,
+        })
+
+      i = i + 1
+      sleep(0)
+
+
+class MockEventStreamer(MockStreamer):
+  def run(self):
+    i = 0
+    while not self._destroy.is_set():
+      if i % 10 != 0:
+        self._queue.put({'transaction': {
+          'tradeId': 615670377, 'accountBalance': 99959.6797, 'price': 173.366, 'side': 'buy',
+          'instrument': 'GBP_JPY', 'interest': -0.0522, 'time': '2014-07-17T18:57:05.000000Z',
+          'units': 4000, 'type': 'TRAILING_STOP_FILLED', 'id': 615936867, 'pl': 12.844,
+          'accountId': 1815754
+        }})
+      else:
+        self._queue.put({'heartbeat': {'time': '2014-07-09T00:00:00.000000Z'}})
+
+      i = i + 1
+      sleep(0)
 
 
 class TestProgram(TestCase):
@@ -73,7 +152,30 @@ class TestProgram(TestCase):
     self.__program.listTrades('1815754')
 
 
+  @patch('program.RateStreamer', MockRateStreamer)
+  @patch('program.EventStreamer', MockEventStreamer)
+  def testRun(self):
+    """Test main infrastructure of the program.
+
+      Notes:
+        This test aimes at executing the majority of paths in the application's main loop.
+    """
+    try:
+      self.__program.start('1815754', 'XAU_USD', 0)
+      # hammer events on the application for one second and quit
+      sleep(1)
+      self.__program.destroy()
+    except KeyboardInterrupt:
+      self.__program.destroy()
+      self.fail('Interrupted')
+
+
 if __name__ == '__main__':
+  # workaround for problem:
+  # "ImportError: Failed to import _strptime because the import lockis held by another thread."
+  from datetime import datetime
+  datetime.strptime('2012-01-01', '%Y-%m-%d')
+
   # enable buffering of output so that in the successful case we do not see any of the messages
   # printed by default (the listed accounts, for instance)
   main(buffer=True)

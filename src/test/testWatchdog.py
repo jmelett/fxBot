0c8127990b104850d01d068b84b737bf6a5eb97d
deso <deso@devnull.com>
2014-07-19 12:31:39 +0100

    fix destroy() problem in Watchdog when start() was not invoked
    This change fixes a the problem that the unregister() method of the Poll
    object is invoked even if no register() call has been made before. This
    case can occur if the destroy() of the watchdog object is invoked on a
    newly created object without having executed start().

---
+++ src/test/testWatchdog.py
@@ -0,0 +1,32 @@
+# testWatchdog.py
+
+#/***************************************************************************
+# *   Copyright (C) 2014 Daniel Mueller                                     *
+# *                                                                         *
+# *   This program is free software: you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation, either version 3 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+# ***************************************************************************/
+
+from watchdog import Watchdog
+from unittest import TestCase, main
+
+
+class TestWatchdog(TestCase):
+  def testDestroyWithoutStart(self):
+    """Verify that we can safely destroy a watchdog object without having it start()ed."""
+    watchdog = Watchdog(currencies=None, queue=None, timeout=None)
+    watchdog.destroy()
+
+
+if __name__ == '__main__':
+  main()

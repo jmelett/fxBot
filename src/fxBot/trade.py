910303a04d1bcb3380d96a692488ee5cd040c364
deso <deso@devnull.com>
2014-07-30 23:01:38 +0100

    TEMPORARY

--- src/fxBot/trade.py
+++ src/fxBot/trade.py
@@ -17,6 +17,77 @@
 # *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 # ***************************************************************************/
 
+# Margin Closeout calculator:
+# If the account currency is the same as the base of the currency pair
+#
+# Buy Position MR = (2mOU) / (2mb + 2mU - U)
+# Sell Position MR = (-2mOU) / (2mb - 2mU -U)
+#
+# Account currency is the same as the quote of the currency pair
+#
+# Buy Position MR = (2m(OU - b)) / U(2m-1)
+# Sell Position MR = (2m(OU + b)) / U(2m+1)
+#
+# If neither the quote nor base of the currency pair is the same as the account currency
+#
+# Buy Position MR = (2m(b - UOH)) / (1 - 2m)UH
+# Sell Position MR = (2m(b + UOH)) / (1 + 2m)UH
+#
+# Where :
+#
+# MR = Margin Closeout rate (approx.)
+# m = Margin Ratio
+# U = Units Held
+# O = Opening Rate of position
+# b = Account Balance
+# H = Quote Home rate (X/USD)
+#
+# Example for typical AUD/USD margin closeout with a 'BUY' position:
+# m = 30 (30:1 Margin ratio)
+# U = 200,000
+# O = 0.55938
+# b = 5700.02
+#
+# We will use the following formula : MR = (2m(OU - b)) / U(2m-1)
+#
+# MR = (2 * (30) * ((0.55938 * 200000) - 5700.02)) / (200000 * (2 * (30) - 1))
+# MR = (60 * (111876 - 5700.02)) / (200000 * 59)
+# MR = (60 * 106175.98) / (11800000)
+# MR = 6370558.8 / 11800000
+# MR = 0.53987
+
+
+# Unit calculator
+# This calculation uses the following formula:
+# Margin Available * (margin ratio) / ({BASE}/{HOME Currency} Exchange Rate)
+# For example, suppose:
+# Home Currency: USD
+# Currency Pair: GBP/CHF
+# Margin Available: 100
+# Margin Ratio : 20:1
+# Base / Home Currency: GBP/USD = 1.584
+# Then,
+# Units = (100 * 20) / 1.584
+# Units = 1262
+
+
+# Profit/Loss calculator
+# This calculation follows the following formula:
+# (Closing Rate - Opening Rate) * (Closing {quote}/{home currency}) * Units
+# For example, suppose:
+#
+# Home Currency: USD
+# Currency Pair: GBP/CHF
+# Base = GBP; Quote = CHF
+# Quote / Home Currency = CHF/USD = 1.1025
+# Opening Rate = 2.1443
+# Closing Rate = 2.1452
+# Units = 1000
+#
+# Then:
+#
+# Profit = (2.1452 - 2.1443) * (1.1025) * 1000
+# Profit = 0.99225 USD
 
 def profitLossRaw(open_rate, close_rate, units):
   """Calculate the profit/loss of a trade in the quoted currency.
@@ -45,3 +116,26 @@ def profitLossRawWithHome(open_rate, close_rate, quote_home_rate, units):
       The profit or loss induced by this trade in the home currency.
   """
   return profitLossRaw(open_rate, close_rate, units) * quote_home_rate
+
+
+class Trade:
+  def __init__(self, server, currency):
+    """XXX.
+
+      Parameters:
+        server    A server object to use for interaction with one of OANDA's servers.
+        currency  String representing a currency pair. Note that the string is directly passed to
+                  the OANDA API without any translation so it must correspond to a string as used by
+                  this API.
+    """
+    self.__server = server
+    self.__currency = currency
+
+
+  def profitLoss(self):
+    """XXX.
+
+      Returns:
+       XXX.
+    """
+    return None
--- src/test/testProfitLoss.py
+++ src/test/testProfitLoss.py
@@ -18,11 +18,56 @@
 # ***************************************************************************/
 
 from trade    import profitLossRaw, profitLossRawWithHome
+from currency import Currency
+from datetime import datetime, timedelta
 from unittest import TestCase, main
 from decimal  import Decimal
+from mock     import patch
+
+
+class MockServer:
+  def __init__(self, ask_price, bid_price):
+    """Create a new MockServer object.
+
+      MockServer is used to virtualize the server objects designed for communication with OANDA's
+      servers in a way to just return predefined values.
+    """
+    self.__ask_price = ask_price
+    self.__bid_price = bid_price
+
+
+  def currentPrices(self, currency):
+    return {
+      'instrument': 'XAU_USD',
+      'time': formatDate(datetime.now()),
+      'ask': self.__ask_price,
+      'bid': self.__bid_price,
+    }
+
+
+  def askPrice(self, ask_price):
+    """Set an ask price to return on next currentPrices() invocation.
+
+      Parameters:
+        ask_price  String specifying the ask price to set.
+    """
+    self.__ask_price = ask_price
+
+
+  def bidPrice(self, bid_price):
+    """Set a bid price to return on next currentPrices() invocation.
+
+      Parameters:
+        bid_price  String specifying the bid price to set.
+    """
+    self.__bid_price = bid_price
 
 
 class TestProfitLoss(TestCase):
+  #def setUp(self):
+  #  self.__server = MockServer()
+
+
   def testProfitLossRaw(self):
     """Test profit calculation in quoted currency."""
     # Home Currency: EUR
@@ -58,6 +103,51 @@ class TestProfitLoss(TestCase):
     profit = profitLossRawWithHome(open_rate, close_rate, quote_home_rate, units)
 
     self.assertEqual(profit, Decimal('0.99225'))
+
+
+
+  #def testLongLoss(self):
+  #  """Test profit calculation for loss on long position."""
+  #  # You see that the rate for USD/JPY is 115.00/05 and decide to buy 10,000 USD. Your trade is
+  #  # executed at 115.05.
+  #  # 10,000 USD * 115.05 = 1,150,500 JPY
+  #  # You bought 10,000 USD and sold 1,150,500 JPY.
+  #  # The market rate of USD/JPY falls to 114.45/50. You decide to sell back 10,000 USD at 114.45.
+  #  # 10,000 USD * 114.45 = 1,144,500 JPY
+  #  # You bought 10,000 USD for 1,150,500 JPY and sold 10,000 USD back for 1,144,500 JPY. The
+  #  # difference is your loss and is calculated as follows: 1,150,500 - 1,144,500 = 6,000 JPY. Note
+  #  # that your loss is in JPY and must be converted back to dollars.
+  #  # To calculate this amount in USD:
+  #  # 6,000 JPY / 114.45 = 52.42 USD or 6,000 * 1/114.45 = 52.42 USD
+  #  self.__server.bid('115.00')
+  #  self.__server.ask('115.05')
+
+  #  currency = Currency(self.__server, 'USD_JPY')
+
+  #  self.__server.bid('114.45')
+  #  self.__server.ask('114.50')
+
+    # You see that the rate for EUR/USD is 0.9517/22 and decide to sell 10,000 EUR. Your trade is
+    # executed at 0.9517.
+    # 10,000 EUR * 0.9517 = 9,517.00 USD
+    # You sold 10,000 EUR and bought 9,517.00 USD.
+    # After you trade, the market rate of EUR/USD decreases to EUR/USD = 0.9500/05. You then buy
+    # back 10,000 EUR at 0.9505.
+    # 10,000 EUR * 0.9505 = 9,505.00 USD
+    # You sold 10,000 EUR for 9,517 USD and bought 10,000 back for 9,505. The difference is your
+    # profit:
+    # 9,517.00 - 9,505.00 = 12.00 USD
+    #currency = Currency(self.__server, 'EUR_USD')
+
+    #self.__server.bid('0.9517')
+    #self.__server.ask('0.9539')
+    #buy_price = currency.currentPrices()
+
+    #self.__server.bid('0.9500')
+    #self.__server.ask('0.9505')
+    #current_price = currency.currentPrices()
+
+    #self.assertEqual(profitLoss(10000, buy_price, current_price, True), Decimal(12.000))
 
 
 if __name__ == '__main__':
